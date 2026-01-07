890cd7088f3ef5d638837d6c0dfbc3718d0cc959
harshsharmaaiinfox <harsh.sharma@aiinfox.com>
2026-01-07 13:14:39 +0530

    Remove overlay elements from collection sidebar and slider components. Implement body scroll toggle functionality in the offcanvas filter component based on menu state. Update styles for improved visibility and responsiveness in the sort component.

--- src/app/components/shop/collection/collection-offcanvas-filter/collection-offcanvas-filter.component.ts
+++ src/app/components/shop/collection/collection-offcanvas-filter/collection-offcanvas-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, DoCheck } from '@angular/core';
 import { Params } from '../../../../shared/interface/core.interface';
 import { AttributeService } from '../../../../shared/services/attribute.service';
 
@@ -7,15 +7,36 @@ import { AttributeService } from '../../../../shared/services/attribute.service'
   templateUrl: './collection-offcanvas-filter.component.html',
   styleUrls: ['./collection-offcanvas-filter.component.scss']
 })
-export class CollectionOffCanvasFilterComponent {
+export class CollectionOffCanvasFilterComponent implements DoCheck {
 
   @Input() filter: Params;
+  private previousOffCanvasState: boolean = false;
 
   constructor(public attributeService: AttributeService) {
+  }
+
+  ngDoCheck() {
+    // Check if offCanvasMenu state has changed
+    if (this.attributeService.offCanvasMenu !== this.previousOffCanvasState) {
+      this.previousOffCanvasState = this.attributeService.offCanvasMenu;
+      this.toggleBodyScroll(!this.attributeService.offCanvasMenu);
+    }
   }
 
   closeCanvasMenu() {
     this.attributeService.offCanvasMenu = false;
   }
-  
+
+  private toggleBodyScroll(enable: boolean) {
+    if (enable) {
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+    } else {
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
+    }
+  }
+
 }
